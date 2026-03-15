daf7d87b028254b5ebbc61fd35b7b44a58e9d4db
guinardpaul <guinardpaul@gmail.com>
2026-03-15 14:53:12 +0100

    Init project with spring initializr

---
+++ src/main/java/com/guinardsolutions/foodquiz/FoodQuizApplication.java
@@ -0,0 +1,13 @@
+package com.guinardsolutions.foodquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodQuizApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FoodQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guinardsolutions/foodquiz/FoodQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guinardsolutions.foodquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
